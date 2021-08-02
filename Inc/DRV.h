988ec5faa6257a434d911ee6538f10bfe7b84543
SerBeetle12 <danila.nosov@phystech.edu>
2021-08-02 14:43:04 +0300

    Tree functions

---
+++ Inc/DRV.h
@@ -0,0 +1,32 @@
+
+#define DRV_REGISTR_0_Up		0x9511
+#define DRV_REGISTR_0_Down	0x9111
+#define DRV_REGISTR_1 			0x40		//0x21
+#define DRV_REGISTR_2 			0x364		//0x3B4
+#define DRV_REGISTR_3 			0x6F82	//0xE800	//0x6F82	//0xE800
+#define DRV_REGISTR_4 			0x247		//0x347		//0x247//0x240
+#define DRV_REGISTR_5 			0x3007
+#define DRV_REGISTR_6 			0x84B1
+#define DRV_REGISTR_7 			0x3BA
+#define DRV_REGISTR_8 			0x12C
+#define DRV_REGISTR_9 			0x250
+#define DRV_REGISTR_10 			0xF064
+#define DRV_REGISTR_11 			0x7D0
+#define DRV_REGISTR_2A 			0
+
+#define MIN_INPUT_PWM_DRV 							900
+#define MAX_INPUT_PWM_DRV 							2150
+#define MAX_ACCELERATION_INPUT_PWM_DRV 	15
+#define MAX_DECELERATION_INPUT_PWM_DRV 	15
+#define DELTA_TIME_PWM_DRV 							80
+
+#define MIN_PWM_DRV_A 			5
+#define MAX_PWM_DRV_A 			99
+#define MIN_PWM_DRV_B 			5
+#define MAX_PWM_DRV_B 			99
+#define MIN_PWM_DRV_C 			5
+#define MAX_PWM_DRV_C 			99
+#define MIN_PWM_DRV_D 			5
+#define MAX_PWM_DRV_D 			99
+#define MAX_SPEED_DRV 			1
+#define MIDDLE_PWM_DRV 			1500
--- Inc/system_func.h
+++ Inc/system_func.h
@@ -2,20 +2,7 @@
 
 #define DEBBUG 1
 
-#define DRV_REGISTR_0_Up		0x9511
-#define DRV_REGISTR_0_Down	0x9111
-#define DRV_REGISTR_1 	0x40		//0x21
-#define DRV_REGISTR_2 	0x364		//0x3B4
-#define DRV_REGISTR_3 	0x6F82	//0xE800	//0x6F82	//0xE800
-#define DRV_REGISTR_4 	0x247		//0x347		//0x247//0x240
-#define DRV_REGISTR_5 	0x3007
-#define DRV_REGISTR_6 	0x84B1
-#define DRV_REGISTR_7 	0x3BA
-#define DRV_REGISTR_8 	0x12C
-#define DRV_REGISTR_9 	0x250
-#define DRV_REGISTR_10 	0xF064
-#define DRV_REGISTR_11 	0x7D0
-#define DRV_REGISTR_2A 	0
+
 
 #define SERVO_1_PWM_ZERO 1500
 
